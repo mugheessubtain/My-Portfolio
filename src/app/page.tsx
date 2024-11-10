492046e96f8d6d62d948293efcf62ca0ddb389b0
Mughees Subtain <160583245+mugheessubtain@users.noreply.github.com>
2024-11-10 19:42:53 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import Main from '@/components/Main'
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+
+export default function Home() {
+
+  return (
+    <div className="background">
+      {/* <div className="circle circle1"></div>
+      <div className="circle circle2"></div> */}
+    <Navbar/>  
+    <Main/>
+    </div>
+  )
+}
